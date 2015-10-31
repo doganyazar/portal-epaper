5acace8add70a6d8a9a05147f4f0a49f3f31e0d7
doganyazar <doganyazar@gmail.com>
2015-10-31 17:36:45 +0100

    Add react bootstrap with browserify support

---
+++ packages/client-deps/client.browserify.js
@@ -0,0 +1,9 @@
+console.log("example1 browserify.js");
+
+ReactBootstrap = require('react-bootstrap'); // export it 'globally' so Meteor is happy and can `api.export` it in package.js
+
+
+if (process.env.NODE_ENV === 'production')
+  console.log('it\'s production!!');
+else
+  console.log('it\'s development...');
